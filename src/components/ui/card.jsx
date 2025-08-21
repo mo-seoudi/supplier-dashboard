3b767572a898d985b09b6ea1091a414e7e635ac7
mo-seoudi <moseoudi@hotmail.com>
2025-08-21 09:06:05 +0400

    Update card.jsx

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -1,7 +1,6 @@
-export function Card({ children }) {
-  return <div className="bg-white shadow rounded">{children}</div>;
+export function Card({ className = "", children }) {
+  return <div className={`rounded-2xl border bg-white ${className}`}>{children}</div>;
 }
-
-export function CardContent({ children, className }) {
-  return <div className={`p-4 ${className}`}>{children}</div>;
+export function CardContent({ className = "", children }) {
+  return <div className={className}>{children}</div>;
 }
